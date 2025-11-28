4676cedd4a6ea874adc9b3c4e2715e6ca6506a3c
Joy <munchinthepool64@gmail.com>
2025-11-28 21:30:41 +0900

    feat: 연관관계 추가

--- src/main/java/com/dearjolly/server/entity/CorrectionSegments.java
+++ src/main/java/com/dearjolly/server/entity/CorrectionSegments.java
@@ -5,13 +5,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,10 +29,10 @@ public class CorrectionSegments {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "correction_segments_id")
+    @Column(name = "correction_segment_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feedback_id", nullable = false)
     private Feedbacks feedback;
 
