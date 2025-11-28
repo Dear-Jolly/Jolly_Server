a251eab3db3fa8af65a6147daa5fdb4f39d6522b
Joy <munchinthepool64@gmail.com>
2025-11-29 00:47:35 +0900

    feat: 연관관계 메서드 추가 및 컨벤션 맞게 수정

--- src/main/java/com/dearjolly/server/entity/CorrectionSegments.java
+++ src/main/java/com/dearjolly/server/entity/CorrectionSegments.java
@@ -12,13 +12,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "CORRECTION_SEGMENTS")
@@ -32,6 +30,7 @@ public class CorrectionSegments {
     @Column(name = "correction_segment_id")
     private Long id;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feedback_id", nullable = false)
     private Feedbacks feedback;
@@ -39,15 +38,29 @@ public class CorrectionSegments {
     @Column(name = "sequence", nullable = false)
     private Integer sequence;
 
-    @Lob
-    @Column(name = "original_text", nullable = false)
+    @Column(name = "original_text", nullable = false, length = 500)
     private String originalText;
 
-    @Lob
-    @Column(name = "corrected_text", nullable = false)
+    @Column(name = "corrected_text", nullable = false, length = 500)
     private String correctedText;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false, length = 10)
     private CorrectionType type;
+
+    private CorrectionSegments(Feedbacks feedback, Integer sequence, String originalText, String correctedText, CorrectionType type) {
+        this.feedback = feedback;
+        this.sequence = sequence;
+        this.originalText = originalText;
+        this.correctedText = correctedText;
+        this.type = type;
+    }
+
+    public static CorrectionSegments create(Feedbacks feedback, Integer sequence, String originalText, String correctedText,
+                                            CorrectionType type) {
+        CorrectionSegments correctionSegment = new CorrectionSegments(feedback, sequence, originalText, correctedText, type);
+        feedback.addCorrectionSegment(correctionSegment);
+        return correctionSegment;
+    }
+
 }
--- src/main/java/com/dearjolly/server/entity/Feedbacks.java
+++ src/main/java/com/dearjolly/server/entity/Feedbacks.java
@@ -1,7 +1,10 @@
 package com.dearjolly.server.entity;
 
+import com.dearjolly.server.entity.enums.Status;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,12 +14,14 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "FEEDBACKS")
@@ -30,7 +35,8 @@ public class Feedbacks {
     @Column(name = "feedback_id")
     private Long id;
 
-    @OneToOne
+    @Setter
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "letter_id", nullable = false)
     private Letters letter;
 
@@ -45,7 +51,7 @@ public class Feedbacks {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "correctionSegments")
+    @OneToMany(mappedBy = "feedback", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CorrectionSegments> correctionSegments = new ArrayList<>();
 
     @PrePersist
@@ -53,4 +59,23 @@ public class Feedbacks {
         this.createdAt = LocalDateTime.now();
     }
 
+    private Feedbacks(Letters letter, String correctedContent, String tip) {
+        this.letter = letter;
+        this.correctedContent = correctedContent;
+        this.tip = tip;
+        this.correctionSegments = new ArrayList<>();
+    }
+
+    public static Feedbacks create(Letters letter, String correctedContent, String tip) {
+        Feedbacks feedback = new Feedbacks(letter, correctedContent, tip);
+        letter.setFeedback(feedback);
+        return feedback;
+    }
+
+    public void addCorrectionSegment(CorrectionSegments segment) {
+        this.correctionSegments.add(segment);
+        if (segment.getFeedback() != this) {
+            segment.setFeedback(this);
+        }
+    }
 }
